f0ec6bbf7f3d0f3d7146c7c917ca90dffd431975
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2020-03-02 10:50:40 +0000

    Update SubNavigation.stories.tsx
    Change Storybook story for the SubNavigation panel to reflect changes to the component

--- stories/SubNavigation.stories.tsx
+++ stories/SubNavigation.stories.tsx
@@ -6,8 +6,8 @@ const stories = storiesOf('Sub Navigation Panel', module);
 
 stories.add('Standard', () => (
   <SubNavigation>
-    <SubNavigation.Item text="Hello world" target="#" active></SubNavigation.Item>
-    <SubNavigation.Item text="Hello world" target="#"></SubNavigation.Item>
-    <SubNavigation.Item text="Hello world" target="#"></SubNavigation.Item>
+    <SubNavigation.Item href="#" aria-current="page">Hello World</SubNavigation.Item>
+    <SubNavigation.Item href="#">Hello World</SubNavigation.Item>
+    <SubNavigation.Item href="#">Hello World</SubNavigation.Item>
   </SubNavigation>
 ));
