a136e83dcaabac5a0d4b3333df9979b26d4f86fe
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2020-04-01 16:48:04 +0000

    Cleanup Icon CSS

--- src/components/accordion-menu/components/Section.tsx
+++ src/components/accordion-menu/components/Section.tsx
@@ -6,6 +6,27 @@ export interface SectionProps extends HTMLProps<HTMLDetailsElement> {
   defaultOpen?: boolean;
 }
 
+const ToggleIcon: React.FC<{ open: boolean }> = ({ open }) => (
+  <svg
+    className="nhsuk-accordion-menu__icon"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 32 32"
+    width={32}
+    height={32}
+    aria-hidden="true"
+  >
+    <circle cx="16" cy="16" r="16" fill="#ffffff" />
+    <path
+      fill="none"
+      stroke="#003087"
+      strokeLinecap="round"
+      strokeMiterlimit="10"
+      strokeWidth="4"
+      d={open ? 'M8,16 h16' : 'M16 8v16M8 16h16'}
+    />
+  </svg>
+);
+
 const Section: React.FC<SectionProps> = ({
   children,
   className,
@@ -44,6 +65,7 @@ const Section: React.FC<SectionProps> = ({
         onClick={onSummaryClick}
       >
         <span className="nhsuk-accordion-menu__section-summary-text">{heading}</span>
+        <ToggleIcon open={isOpen} />
       </summary>
       {children}
     </details>
