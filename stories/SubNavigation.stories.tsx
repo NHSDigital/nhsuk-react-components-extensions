746d9d2589714e2bce3d38ec63e046d25091e4fe
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2020-04-01 10:21:02 +0000

    Fixed exports test

--- stories/SubNavigation.stories.tsx
+++ stories/SubNavigation.stories.tsx
@@ -9,19 +9,19 @@ stories.add('Standard', () => {
   return (
     <SubNavigation>
       <SubNavigation.Item
-        aria-current={(activeTab === 1) ? 'page' : false}
+        aria-current={activeTab === 1 ? 'page' : false}
         onClick={() => setActiveTab(1)}
       >
         Tab 1
       </SubNavigation.Item>
       <SubNavigation.Item
-        aria-current={(activeTab === 2) ? 'page' : false}
+        aria-current={activeTab === 2 ? 'page' : false}
         onClick={() => setActiveTab(2)}
       >
         Tab 2
       </SubNavigation.Item>
       <SubNavigation.Item
-        aria-current={(activeTab === 3) ? 'page' : false}
+        aria-current={activeTab === 3 ? 'page' : false}
         onClick={() => setActiveTab(3)}
       >
         Tab 3
