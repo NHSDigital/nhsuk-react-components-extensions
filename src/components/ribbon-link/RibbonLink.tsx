ad79913affddc5159163f20905b6c60f1b3ea9a0
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2020-04-02 11:06:21 +0000

    WIP: Add ribbon link component

---
+++ src/components/ribbon-link/RibbonLink.tsx
@@ -0,0 +1,32 @@
+import React, { HTMLProps } from 'react';
+import classNames from 'classnames';
+import { Icons } from 'nhsuk-react-components';
+import Bar from './components/Bar';
+
+type RibbonFlavours = 'hot' | 'mild' | 'cool';
+
+interface RibbonProps extends HTMLProps<HTMLButtonElement> {
+  flavour: RibbonFlavours;
+  type?: 'button' | 'submit' | 'reset';
+}
+
+interface RibbonLink extends React.FC<RibbonProps> {
+  Bar: React.FC<HTMLProps<HTMLDivElement>>;
+}
+
+const RibbonLink: RibbonLink = ({ children, flavour, className, ...rest }) => (
+  <button
+    className={classNames('nhsuk-ribbon-link', `nhsuk-ribbon-link--${flavour}`, className)}
+    type="button"
+    {...rest}
+  >
+    <Icons.ArrowRightCircle
+      className={`nhsuk-ribbon-link__icon nhsuk-ribbon-link__icon--${flavour}`}
+    />
+    <span>{children}</span>
+  </button>
+);
+
+RibbonLink.Bar = Bar;
+
+export default RibbonLink;
---
+++ stories/RibbonLink.stories.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { RibbonLink } from '../src';
+
+const stories = storiesOf('RibbonLink', module);
+
+stories
+  .add('Cool Ribbon', () => (
+    <div style={{ padding: 20 }}>
+      <RibbonLink flavour="cool">Cool Ribbon</RibbonLink>
+    </div>
+  ))
+  .add('Mild Ribbon', () => (
+    <div style={{ padding: 20 }}>
+      <RibbonLink flavour="mild">Mild Ribbon</RibbonLink>
+    </div>
+  ))
+  .add('Hot Ribbon', () => <RibbonLink flavour="hot">Hot Ribbon</RibbonLink>)
+  .add('RibbonLink Bar', () => (
+    <RibbonLink.Bar className="ribbonlink-demo">
+      <RibbonLink flavour="cool">Cool Ribbon</RibbonLink>
+      <RibbonLink flavour="mild">Mild Ribbon</RibbonLink>
+      <RibbonLink flavour="hot">Hot Ribbon</RibbonLink>
+    </RibbonLink.Bar>
+  ));
