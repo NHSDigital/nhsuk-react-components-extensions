6a67a6a3408638a577710238dc86b5a14e95f5e0
Harry Coulter <harrycoulter@Harrys-MacBook-Pro.local>
2020-07-10 16:04:24 +0100

    accounting for falsy text values

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,13 +1,13 @@
 import React, { HTMLProps } from 'react';
 
 interface Props extends HTMLProps<HTMLDivElement> {
-  text?: string;
+  text?: string | false;
 }
 
 const Loading: React.FC<Props> = ({ text, ...rest }: Props) => (
   <div {...rest}>
     <div className="nhsuk-loader__container">
-      <p className="nhsuk-heading-l">{text}</p>
+      {text && <p className="nhsuk-heading-l">{text}</p>}
     </div>
 
     <div className="nhsuk-loader__container">
--- src/components/loading/__tests__/Loading.tests.tsx
+++ src/components/loading/__tests__/Loading.tests.tsx
@@ -8,14 +8,21 @@ describe('loading', () => {
       <Loading text="I am testing the loading component" />
     );
     expect(component).toMatchSnapshot();
-    expect(component.text).toBe("I am testing the loading component");
+    expect(component.text()).toBe("I am testing the loading component");
     component.unmount();
   });
 
   it('renders with default text if none supplied', () => {
     const component = shallow(<Loading />);
     expect(component).toMatchSnapshot();
-    expect(component.text).toBe("Loading...");
+    expect(component.text()).toBe("Loading...");
+    component.unmount();
+  });
+
+  it('renders without text if false value supplied', () => {
+    const component = shallow(<Loading text=""/>);
+    expect(component).toMatchSnapshot();
+    expect(component.children('p')).toHaveLength(0);
     component.unmount();
   });
 });
