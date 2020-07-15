700655f09188b5f51221a14f374dba47df5a909b
Thomas Judd-Cooper <thomas.judd-cooper1@nhs.net>
2020-07-15 12:36:56 +0100

    Refactor loading component

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ export { default as SubNavigation } from './components/sub-navigation';
 export { default as TabSet } from './components/tab-set';
 export { default as Tag } from './components/tag';
 export { default as Tooltip } from './components/tooltip';
-export { default as Loading } from './components/loading';
+export { default as Loading } from './components/loading-spinner';
 export { WarningIcon } from './components/icons';
--- stories/Loading.stories.tsx
+++ stories/Loading.stories.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { storiesOf } from '@storybook/react';
 import { Loading } from '../src';
 
 const stories = storiesOf('Loading spinner', module);
 
 stories
-  .add('Basic', () => (
-    <Loading />
-  ))
-  .add('With custom text', () => (
-    <Loading text="Loading patients" />
-  ))
-  .add('Without text', () => (
-    <Loading text={false} />
-  ));
+  .add('Basic', () => <Loading />)
+  .add('With custom text', () => <Loading text="Loading patients" />)
+  .add('Without text', () => <Loading text={false} />)
+  .add('Without backdrop', () => <Loading backdrop={false} />)
+  .add('With custom sizing', () => <Loading width={100} height={100} text={false} />)
+  .add('With programmatic control', () => {
+    const [isShown, setIsShown] = useState<boolean>(true);
+
+    useEffect(() => {
+      const timeout = setTimeout(() => setIsShown(!isShown), 3000);
+      return () => clearTimeout(timeout);
+    }, [isShown]);
+
+    return <Loading shown={isShown} />;
+  });
