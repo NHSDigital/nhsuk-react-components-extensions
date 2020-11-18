8c29a29a33b6af9035517537af0028a6fdec3cc3
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2020-11-18 17:50:13 +0000

    added more events to storybook example

--- stories/Timeline.stories.tsx
+++ stories/Timeline.stories.tsx
@@ -24,6 +24,22 @@ const storybookEvents = [
       "CRM Case Number: CAS-12345-ABCDE",
       "Comments: Requested via the GP form"
     ]
+  },
+  {
+    title: "Next test due date changed",
+    instigator: "System",
+    date: new Date(),
+    description: [
+      "Next test due date changed to 10-Oct-2021"
+    ]
+  },
+  {
+    title: "Test result added",
+    instigator: "System (Edifax)",
+    date: new Date(),
+    description: [
+      "Test Result: (39S) Low-grade dyskaryosis, HPV positive, Repeat Advised"
+    ]
   }
 ]
 
