dc8f9f2db62d2cf11602aa7074faf293233dd92c
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2020-11-19 15:14:27 +0000

    improved accessibility and added date formatting

--- src/components/timeline/components/Event.tsx
+++ src/components/timeline/components/Event.tsx
@@ -7,12 +7,24 @@ export interface EventProps {
   description?: Array<string>;
 }
 
+const dateOptions = {
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+  hour12: true,
+};
+
 const Event: React.FC<EventProps> = ({ title, instigator, date, description = [] }) => (
   <div className="nhsuk-timeline__event nhsuk-u-margin-bottom-2">
-    <h2 className="nhsuk-timeline__title">{title}</h2>
-    <p className="nhsuk-timeline__by"> by {instigator}</p>
+    <h2 className="nhsuk-timeline__title">
+      {title}
+      <span className="nhsuk-timeline__by"> by {instigator}</span>
+    </h2>
     <p className="nhsuk-timeline__date">
-      <time dateTime={date.toString()}>{date.toString()}</time>
+      <time dateTime={date.toString()}>{date.toLocaleString('en-GB', dateOptions)}</time>
     </p>
     {description.map((descriptionItem, index) => (
       <p key={index.toString()} className="nhsuk-timeline__description-item">
--- stories/Timeline.stories.tsx
+++ stories/Timeline.stories.tsx
@@ -9,7 +9,7 @@ const storybookEvents = [
   {
     title: 'Result sent',
     instigator: 'System',
-    date: new Date('2019-11-19'),
+    date: new Date('2019-11-19 19:49:10'),
     description: [
       'Test Result: (39S) Low-grade dyskaryosis, HPV positive, Repeat Advised',
       'Test Date: 19-Oct-2020, 9:00:00 am',
@@ -18,7 +18,7 @@ const storybookEvents = [
   {
     title: 'Patient deferred',
     instigator: 'James Smith',
-    date: new Date('2019-11-19'),
+    date: new Date('2019-11-19 16:28:57'),
     description: [
       'Defer Reason: Pregnancy',
       'CRM Case Number: CAS-12345-ABCDE',
@@ -28,13 +28,13 @@ const storybookEvents = [
   {
     title: 'Next test due date changed',
     instigator: 'System',
-    date: new Date('2019-11-19'),
+    date: new Date('2019-11-19 11:59:59'),
     description: ['Next test due date changed to 10-Oct-2021'],
   },
   {
     title: 'Test result added',
     instigator: 'System (Edifax)',
-    date: new Date('2019-11-19'),
+    date: new Date('2019-11-19 09:12:42'),
     description: ['Test Result: (39S) Low-grade dyskaryosis, HPV positive, Repeat Advised'],
   },
 ];
