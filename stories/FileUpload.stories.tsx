f088d229da0e901dba79c5d2c4628ff4cc2cb8de
Kai Spencer <kaispencer98@gmail.com>
2021-08-12 23:26:24 +0100

    Add stories

--- stories/FileUpload.stories.tsx
+++ stories/FileUpload.stories.tsx
@@ -2,8 +2,22 @@
 import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { FileUpload } from '../src';
+
 const stories = storiesOf('File Upload', module);
 
 stories
-  .add('Standard', () => <FileUpload>Upload a file</FileUpload>)
-  .add('With Error', () => <FileUpload error="Invalid file type">Upload a file</FileUpload>);
+  .add('Standard', () => (
+    <div className="fileupload-demo">
+      <FileUpload>Upload a file</FileUpload>
+    </div>
+  ))
+  .add('With Error', () => (
+    <div className="fileupload-demo">
+      <FileUpload error="Invalid file type">Upload a file</FileUpload>
+    </div>
+  ))
+  .add('With Hint', () => (
+    <div className="fileupload-demo">
+      <FileUpload hint="Maximum file size 2GB">Upload a File</FileUpload>
+    </div>
+  ));
