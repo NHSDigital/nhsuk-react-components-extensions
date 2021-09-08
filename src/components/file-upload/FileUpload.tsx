de28c3d6dce2f4848a1cf9afd211b4c3c8775cfc
Kai <kaispencer98@gmail.com>
2021-09-08 11:50:15 +0100

    File Upload review comments, simplification using existing components.

--- src/components/file-upload/FileUpload.tsx
+++ src/components/file-upload/FileUpload.tsx
@@ -1,40 +1,19 @@
-import { Hint } from 'nhsuk-react-components';
+import classNames from 'classnames';
+import { ErrorMessage, Hint, Label } from 'nhsuk-react-components';
 import React, { HTMLProps } from 'react';
 
 interface FileUploadProps extends HTMLProps<HTMLDivElement> {
   error?: string;
   hint?: string;
-  children: React.ReactNode;
 }
 
-const FileUpload: React.FC<FileUploadProps> = ({
-  error,
-  hint,
-  children,
-  ...rest
-}: FileUploadProps) => {
+const FileUpload: React.FC<FileUploadProps> = ({ error, hint, children, id, ...rest }) => {
   return (
-    <div className={`nhsuk-form-group ${error ? 'nhsuk-form-group--error' : ''}`} {...rest}>
-      <label htmlFor="file-upload-label" className="nhsuk-label">
-        {children}
-      </label>
-      {error && (
-        <span
-          id="file-select-error-id"
-          data-test-id="file-select-error"
-          className="nhsuk-error-message"
-        >
-          <span className="nhsuk-visually-hidden">Error: </span>
-          {error}
-        </span>
-      )}
+    <div className={classNames('nhsuk-form-group', { 'nhsuk-form-group--error': error })} {...rest}>
+      <Label htmlFor={id}>{children}</Label>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
       {hint && <Hint data-test-id="file-hint">{hint}</Hint>}
-      <input
-        aria-describedby="file-upload-label"
-        id="file-upload"
-        className="nhsuk-file-upload"
-        type="file"
-      />
+      <input aria-describedby={id} id="file-upload" className="nhsuk-file-upload" type="file" />
     </div>
   );
 };
