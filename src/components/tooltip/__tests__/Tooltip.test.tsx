5ffcc6b554de426dd0c12800db1b42aa9391e27e
alice-bell <alice.bell10@nhs.net>
2024-11-12 16:30:45 +0000

    NCRS-3363: updated Tooltip tests (check snaps)

--- src/components/tooltip/__tests__/Tooltip.test.tsx
+++ src/components/tooltip/__tests__/Tooltip.test.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
-import { mount } from 'enzyme';
+// import { mount } from 'enzyme';
+import {render} from '@testing-library/react'
 import Tooltip from '..';
 
 describe('Tooltip', () => {
   it('Matches Snapshot', () => {
-    const component = mount(<Tooltip tooltip="Tooltip!">Hover Over Me</Tooltip>);
+    const component = render(<Tooltip tooltip="Tooltip!">Hover Over Me</Tooltip>);
     expect(component).toMatchSnapshot();
-    component.unmount();
   });
 
   it('Passes through IDs', () => {
-    const componentWithoutId = mount(<Tooltip tooltip="Tooltip!">Hover Over Me</Tooltip>);
-    const componentWithId = mount(
+    const componentWithoutId = render(<Tooltip tooltip="Tooltip!">Hover Over Me</Tooltip>);
+    const componentWithId = render(
       <Tooltip tooltip="Tooltip!" id="test-id">
         Hover Over Me
       </Tooltip>,
     );
 
-    expect(componentWithoutId.find('div').prop('aria-labelledby')).toBe(undefined);
-    expect(componentWithId.find('div').prop('aria-labelledby')).toBe('test-id-tooltip-text');
+    // 3363 - note - do these need to be more complex???
+    expect(componentWithoutId.container.querySelector('#test-id')).toBeNull();
+    expect(componentWithoutId.container).toMatchSnapshot();
 
-    expect(componentWithId.find('span').prop('id')).toBe('test-id-tooltip-text');
-    expect(componentWithoutId.find('span').prop('id')).toBe(undefined);
+    expect(componentWithId.container.querySelector('#test-id-tooltip-text')).toBeTruthy();
+    expect(componentWithId.container).toMatchSnapshot();
 
-    componentWithId.unmount();
-    componentWithoutId.unmount();
-  });
+  })
+
+  //   expect(componentWithoutId.find('div').prop('aria-labelledby')).toBe(undefined);
+  //   expect(componentWithId.find('div').prop('aria-labelledby')).toBe('test-id-tooltip-text');
+
+  //   expect(componentWithId.find('span').prop('id')).toBe('test-id-tooltip-text');
+  //   expect(componentWithoutId.find('span').prop('id')).toBe(undefined);
+
+  //   componentWithId.unmount();
+  //   componentWithoutId.unmount();
+  // });
 });
