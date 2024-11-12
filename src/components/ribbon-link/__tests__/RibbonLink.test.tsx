16673898279e727905fba3257b2cbc46396c5564
alice-bell <alice.bell10@nhs.net>
2024-11-12 16:50:32 +0000

    NCRS-3363: update RibbonLink tests

--- src/components/ribbon-link/__tests__/RibbonLink.test.tsx
+++ src/components/ribbon-link/__tests__/RibbonLink.test.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+// import { shallow } from 'enzyme';
+import {render} from '@testing-library/react'
 import RibbonLink from '..';
 
 describe('RibbonLink', () => {
   it('matches snapshot', () => {
-    const coolRibbon = shallow(<RibbonLink flavour="cool">Cool</RibbonLink>);
-    const mildRibbon = shallow(<RibbonLink flavour="mild">Mild</RibbonLink>);
-    const hotRibbon = shallow(<RibbonLink flavour="hot">Hot</RibbonLink>);
+    const coolRibbon = render(<RibbonLink flavour="cool">Cool</RibbonLink>);
+    const mildRibbon = render(<RibbonLink flavour="mild">Mild</RibbonLink>);
+    const hotRibbon = render(<RibbonLink flavour="hot">Hot</RibbonLink>);
 
-    expect(coolRibbon.prop('className')).toBe('nhsuk-ribbon-link nhsuk-ribbon-link--cool');
-    expect(mildRibbon.prop('className')).toBe('nhsuk-ribbon-link nhsuk-ribbon-link--mild');
-    expect(hotRibbon.prop('className')).toBe('nhsuk-ribbon-link nhsuk-ribbon-link--hot');
+    expect(coolRibbon.container.querySelector('button')?.className).toBe('nhsuk-ribbon-link nhsuk-ribbon-link--cool');
+    expect(mildRibbon.container.querySelector('button')?.className).toBe('nhsuk-ribbon-link nhsuk-ribbon-link--mild');
+    expect(hotRibbon.container.querySelector('button')?.className).toBe('nhsuk-ribbon-link nhsuk-ribbon-link--hot');
 
+    // 3363 - check snapshots
     expect(coolRibbon).toMatchSnapshot();
     expect(mildRibbon).toMatchSnapshot();
     expect(hotRibbon).toMatchSnapshot();
-
-    coolRibbon.unmount();
-    mildRibbon.unmount();
-    hotRibbon.unmount();
   });
 
   describe('RibbonLink.Bar', () => {
     it('matches snapshot', () => {
-      const bar = shallow(<RibbonLink.Bar />);
+      // 3363 - check snapshot
+      const bar = render(<RibbonLink.Bar />);
       expect(bar).toMatchSnapshot();
-      bar.unmount();
     });
   });
 });
