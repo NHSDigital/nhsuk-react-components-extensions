c878db828da0968255d82cd87300fa4391275900
alice-bell <alice.bell10@nhs.net>
2024-11-12 14:50:55 +0000

    NCRS-3363: updated TabSet tests

--- src/components/tab-set/__tests__/TabSet.test.tsx
+++ src/components/tab-set/__tests__/TabSet.test.tsx
@@ -4,47 +4,43 @@ import {render} from '@testing-library/react'
 import TabSet from '..';
 
 describe('TabSet', () => {
+  // 3363 - check snapshot
   it('matches snapshot', () => {
     const component = render(<TabSet />)
     expect(component).toMatchSnapshot();
   });
 
-  // describe('Tab', () => {
-  //   it('matches snapshot', () => {
-  //     const component = render(<TabSet.Tab />);
-  //     expect(component).toMatchSnapshot();
-  //     component.unmount();
-  //   });
+  // 3363 - check snapshot
+  describe('Tab', () => {
+    it('matches snapshot', () => {
+      const component = render(<TabSet.Tab />);
+      expect(component).toMatchSnapshot();
+    });
 
-  //   it('applies correct classes', () => {
-  //     const component = render(
-  //       <TabSet>
-  //         <TabSet.Tab id="normal">Text</TabSet.Tab>
-  //         <TabSet.Tab id="disabled" disabled>
-  //           Disabled
-  //         </TabSet.Tab>
-  //         <TabSet.Tab id="active" active>
-  //           Active
-  //         </TabSet.Tab>
-  //         <TabSet.Tab id="empty" empty>
-  //           Empty
-  //         </TabSet.Tab>
-  //       </TabSet>,
-  //     );
+    it('applies correct classes', () => {
+      const component = render(
+        <TabSet>
+          <TabSet.Tab id="normal">Text</TabSet.Tab>
+          <TabSet.Tab id="disabled" disabled>
+            Disabled
+          </TabSet.Tab>
+          <TabSet.Tab id="active" active>
+            Active
+          </TabSet.Tab>
+          <TabSet.Tab id="empty" empty>
+            Empty
+          </TabSet.Tab>
+        </TabSet>,
+      );
 
-  //     const renderedComponent = component.render();
-  //     expect(renderedComponent.find('#normal').prop('class')).toBe('nhsuk-tab-set__tab');
-  //     expect(renderedComponent.find('#disabled').prop('class')).toBe(
-  //       'nhsuk-tab-set__tab nhsuk-tab-set__tab--disabled',
-  //     );
-  //     expect(renderedComponent.find('#active').prop('class')).toBe(
-  //       'nhsuk-tab-set__tab nhsuk-tab-set__tab--active',
-  //     );
-  //     expect(renderedComponent.find('#empty').prop('class')).toBe(
-  //       'nhsuk-tab-set__tab nhsuk-tab-set__tab--empty',
-  //     );
-
-  //     component.unmount();
-  //   });
-  // });
+      expect(component.container.querySelector('#normal')?.className).toBe(
+        'nhsuk-tab-set__tab')
+      expect(component.container.querySelector('#disabled')?.className).toBe(
+        'nhsuk-tab-set__tab nhsuk-tab-set__tab--disabled')
+      expect(component.container.querySelector('#active')?.className).toBe(
+        'nhsuk-tab-set__tab nhsuk-tab-set__tab--active')
+      expect(component.container.querySelector('#empty')?.className).toBe(
+        'nhsuk-tab-set__tab nhsuk-tab-set__tab--empty')
+    });
+  });
 });
