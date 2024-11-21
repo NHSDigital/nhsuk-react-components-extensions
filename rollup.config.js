fd169a39b78010d760c82fe25733f0b49c536742
alice-bell <alice.bell10@nhs.net>
2024-11-21 09:00:29 +0000

    NCRS-3363: updated nhsuk-react-components and rollup config, got tests tentatively passing

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,28 @@
-import typescriptPlugin from 'rollup-plugin-typescript2';
-import typescript from 'typescript';
+// import typescriptPlugin from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
+// import typescript from 'typescript';
 import pkg from './package.json';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.ts',
-  output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
-  ],
+  // output: [
+  //   { file: pkg.main, format: 'cjs' },
+  //   { file: pkg.module, format: 'es' },
+  // ],
+  output: {
+    // 3363 - NOTE - look at this, is it the correct outdir?
+    // makes the tests pass though
+    // should it be lib or dist?  Lib matches current Typescript compiler option for outDir
+    dir: "./lib",
+    // dir
+    format: 'cjs'
+  },
   external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-  // external: [],
-  // external: [/node_modules/],
-  // external: ['nhsuk-react-components/dist/lib/util/FormGroup'],
-  // external: ['nhsuk-react-components'],
   plugins: [
+    typescript(),
     nodeResolve(),
     commonjs(),
-    typescriptPlugin({
-      typescript,
-      exclude: ['**/*.test.d.ts'],
-    })
   ],
 };
