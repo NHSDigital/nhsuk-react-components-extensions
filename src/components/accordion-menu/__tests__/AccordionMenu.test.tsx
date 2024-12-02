a57db65b3a4aee8968ecc1f832432ec96da8f78a
alice-bell <alice.bell10@nhs.net>
2024-12-02 08:57:29 +0000

    NCRS-3363: AccordionMenu tests

--- src/components/accordion-menu/__tests__/AccordionMenu.test.tsx
+++ src/components/accordion-menu/__tests__/AccordionMenu.test.tsx
@@ -25,70 +25,41 @@ describe('AccordionMenu', () => {
         const component = render(
           <AccordionMenu.Section id="accordion" heading="Heading" defaultOpen />,
         );
-        // const event = {
-        //   preventDefault: jest.fn(),
-        // };
-  
-        expect(component.container.querySelector('#accordion')).toBe(4)
+
+
+        expect(component.container.querySelector('#accordion')).toHaveProperty('open',true)
+
+        component.getByText("Heading").click()
+        expect(component.container.querySelector('#accordion')).toHaveProperty('open',false)
+
+        component.getByText("Heading").click()
+
+        expect(component.container.querySelector('#accordion')).toHaveProperty('open',true)
+              });
+
+      it('handles open={true}', () => {
+        const component = render(
+          <AccordionMenu.Section id="accordion" heading="Heading" open />,
+        );
+
+        expect(component.container.querySelector('#accordion')).toHaveProperty('open',true)
+
+        component.getByText("Heading").click()
+
+        expect(component.container.querySelector('#accordion')).toHaveProperty('open',true)
+      });
+
+      it('handles open={false}', () => {
+        const component = render(
+          <AccordionMenu.Section id="accordion" heading="Heading" open={false} />,
+        );
+        expect(component.container.querySelector('#accordion')).toHaveProperty('open',false)
+
+        component.getByText("Heading").click()
+
+        expect(component.container.querySelector('#accordion')).toHaveProperty('open',false)
       });
   
-
-    // 3363 - fix this one!
-    // it('handles defaultOpen', () => {
-    //   const component = render(
-    //     <AccordionMenu.Section id="accordion" heading="Heading" defaultOpen />,
-    //   );
-    //   const event = {
-    //     preventDefault: jest.fn(),
-    //   };
-
-    //   expect(component.container.querySelector('#accordion')).toBe(4)
-
-    //   // component.getByText('Heading').click();
-      // component.container.querySelector('.nhsuk-accordion-menu__icon')?.click()
-      // expect(event.preventDefault).toHaveBeenCalledTimes(1);
-
-      // expect(component.find('#accordion').getElement().props.open).toBe(true);
-      // component.find('summary').simulate('click', event);
-      // expect(event.preventDefault).toHaveBeenCalledTimes(1);
-      // expect(component.find('#accordion').getElement().props.open).toBe(false);
-      // component.find('summary').simulate('click', event);
-      // expect(event.preventDefault).toHaveBeenCalledTimes(2);
-      // expect(component.find('#accordion').getElement().props.open).toBe(true);
-      // component.unmount();
-    });
-
-    // it('handles open={true}', () => {
-    //   const component = shallow(<AccordionMenu.Section id="accordion" heading="Heading" open />);
-    //   const event = {
-    //     preventDefault: jest.fn(),
-    //   };
-    //   expect(component.find('#accordion').getElement().props.open).toBe(true);
-    //   component.find('summary').simulate('click', event);
-    //   expect(event.preventDefault).toHaveBeenCalledTimes(1);
-    //   expect(component.find('#accordion').getElement().props.open).toBe(true);
-    //   component.find('summary').simulate('click', event);
-    //   expect(event.preventDefault).toHaveBeenCalledTimes(2);
-    //   expect(component.find('#accordion').getElement().props.open).toBe(true);
-    //   component.unmount();
-    // });
-
-    // it('handles open={false}', () => {
-    //   const component = shallow(
-    //     <AccordionMenu.Section id="accordion" heading="Heading" open={false} />,
-    //   );
-    //   const event = {
-    //     preventDefault: jest.fn(),
-    //   };
-    //   expect(component.find('#accordion').getElement().props.open).toBe(false);
-    //   component.find('summary').simulate('click', event);
-    //   expect(event.preventDefault).toHaveBeenCalledTimes(1);
-    //   expect(component.find('#accordion').getElement().props.open).toBe(false);
-    //   component.find('summary').simulate('click', event);
-    //   expect(event.preventDefault).toHaveBeenCalledTimes(2);
-    //   expect(component.find('#accordion').getElement().props.open).toBe(false);
-    //   component.unmount();
-    // });
   });
   describe('Link', () => {
     it('matches snapshot', () => {
@@ -97,4 +68,4 @@ describe('AccordionMenu', () => {
       expect(component.container.textContent).toBe('Test');
     });
   });
-// });
+});
