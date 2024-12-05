f1145892eb9cebc3c5f1051a9ef80a1942ef5118
alice-bell <alice.bell10@nhs.net>
2024-12-05 09:14:25 +0000

    NCRS-3363: got rid of rollup build warnings by adding source maps

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,8 +6,8 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   input: 'src/index.ts',
   output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' }
+    { file: pkg.main, format: 'cjs', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true }
   ],
   external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
   plugins: [
