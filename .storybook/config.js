3df23a20ed1e7517f75fc8ae8836722f419e4cb5
alice-bell <alice.bell10@nhs.net>
2024-12-05 10:09:24 +0000

    NCRS-3363: partly fixed Storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,9 @@
 import { configure, addParameters } from '@storybook/react';
 import '../src/all.scss';
 import './_storybook.scss';
+import '../src/styles/core.scss';
+import '../src/styles/variables.scss';
+
 import NHSTheme from './theme';
 
 addParameters({
